0edc6b124ffbcd6b7065e7705c2ba104cce56f79
DagiWorkSE <101919935+DagiWorkSE@users.noreply.github.com>
2023-02-13 23:47:38 +0300

    Created And Tested Adding to the Todo Table

--- server/index.js
+++ server/index.js
@@ -4,8 +4,19 @@ const cors = require("cors");//user to help in communication between domain like
 const todo_db = require("./db_conn");// my databse connection
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());// request.body accessable by this
 
+//Add Todo Route//
+app.post("/create_todo",async(req,res)=>{
+    try {
+        const {description} = req.body;//Get Description from Request Body 
+        const create_todo = await todo_db.query("INSERT INTO todo (description) VALUES ($1) Returning *",[description]);//basic SQL query
+
+        res.json(create_todo.rows[0]);//to return inserted item
+    } catch (err) {
+        console.log(err.message)
+    }
+});
 app.listen(5000,()=>{
     console.log("Server Running on port 5000");
 });
