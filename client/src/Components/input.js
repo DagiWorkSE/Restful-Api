229be32944c7bc384f038ef0e37a9ee257604142
DagiWorkSE <101919935+DagiWorkSE@users.noreply.github.com>
2023-02-22 23:04:14 +0300

    connected component with backend

--- client/src/Components/input.js
+++ client/src/Components/input.js
@@ -2,10 +2,25 @@ import React,{Fragment,useState} from 'react';//fragment used to create fragment
 const Inputtodo = ()=>{//component creation
 const [description,setDescription] = useState("default description");
 
+const onFormSubmit = async e =>{
+    e.preventDefault();//so page dont refresh on submit
+    try {
+        const body = {description};//get description variable to json body
+        const response = await fetch("http://localhost:5000/add_todo",{//fetch inserts goto,properties
+            method:"POST",
+            headers:{"Content-type":"application/json"}, 
+            body:JSON.stringify(body)//convert text to json
+        });
+        console.log(response);
+
+    } catch (err) {
+        console.error(err.message);
+    }
+}
     return(
         <Fragment>
             <h1 className='text-center mt-4'>PERN Todo List</h1>
-            <form className='d-flex mt-4'>
+            <form className='d-flex mt-4' onSubmit={onFormSubmit}>
                 <input type="text" className='form-control' value={description} onChange={e => setDescription(e.target.value)}/>
                 <button className='btn btn-primary'>Add</button>
             </form>
