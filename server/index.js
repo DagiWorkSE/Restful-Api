b850cda65ee6d391995b2b253dffd033276e00ee
DagiWorkSE <101919935+DagiWorkSE@users.noreply.github.com>
2023-02-22 22:36:03 +0300

    API part done with basic CRUD operations

--- server/index.js
+++ server/index.js
@@ -17,6 +17,57 @@ app.post("/create_todo",async(req,res)=>{
         console.log(err.message)
     }
 });
+//GET ALL DATA
+app.get("/get_todos", async(req,res)=>{
+    try {
+        const get_todos = await pool.query("SELECT * FROM todo");//no returning cause Select already returns
+
+        res.json(get_todos.rows);// all rows to be returned
+    } catch (err) {
+        console.error(err.message);
+        
+    }
+});
+
+//GET Specific Item
+
+app.get("/get_todos/:id", async(req,res)=>{
+try {
+    const {id} = req.params;//req.params is given parameters like 'http://localhost:5000/get_todos/2'
+    const get_todos = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]);
+
+    res.json(get_todos.rows);
+} catch (err) {
+    console.error(err.message);
+    
+}
+});
+
+//Update a todo
+app.put("/update_todo/:id", async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        const update_todo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2 Returning *",[description,id]);
+
+        res.json(update_todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+
+//Deleting A Todo
+app.delete('/remove_todo/:id', async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const remove_todo = await pool.query("DELETE FROM todo WHERE todo_id = $1",[id]);
+        res.json(remove_todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.listen(5000,()=>{
     console.log("Server Running on port 5000");
 });
